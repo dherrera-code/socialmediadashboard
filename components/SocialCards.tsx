2696ba364540ef02e4cf3677ec30464b799a4baf
dherrera-code <dherr929@gmail.com>
2026-04-01 09:44:29 -0700

    style toggle button with custom theme and created logic to shorted follower string when its over 10000!

--- components/SocialCards.tsx
+++ components/SocialCards.tsx
@@ -21,7 +21,19 @@ const SocialCards = () => {
     if (socialMedia == "youtube") return "border-t-4 border-x-0 border-b-0 !border-red-600"
 
     return ""
+  }
 
+  const handleFollowerCount = (count: number) => {
+    let countString = count.toString()
+
+    if(count > 10000){
+      console.log(countString)
+      // 10 000
+      countString = countString.slice(0, countString.length - 3)
+      console.log(countString + "K")
+      countString = countString + "k";
+    }
+    return countString;
   }
 
   useEffect(() => {
@@ -40,7 +52,7 @@ const SocialCards = () => {
               <p className="ps-3 text-sm dark:text-gray-400">{item.username}</p>
             </div>
             <div className="flex flex-col justify-center items-center py-2">
-              <h1 className="text-6xl font-bold">{item.followerCount}</h1>
+              <h1 className="text-6xl font-bold">{handleFollowerCount(item.followerCount)}</h1>
               <p className="text-gray-400">F O L L O W E R S</p>
             </div>
             <div className="flex justify-center items-center gap-1">
@@ -51,68 +63,6 @@ const SocialCards = () => {
         </div>
 
       ))}
-      {/* 
-          <Card className="dark:bg-[#252A41] hover:dark:bg-[#3a4164] cursor-pointer border-t-4 border-x-0 border-b-0 !border-blue-300">
-            <div className="flex justify-center items-center">
-              <img className="w-7" src="/images/icon-facebook.svg" alt="Facebook Logo!" />
-              <p className="ps-3 text-sm dark:text-gray-400">username here!</p>
-            </div>
-            <div className="flex flex-col justify-center items-center py-2">
-              <h1 className="text-6xl font-bold">1987</h1>
-              <p className="text-gray-400">F O L L O W E R S</p>
-            </div>
-            <div className="flex justify-center items-center gap-1">
-              <img className="w-3 h-fit" src="/images/icon-up.svg" alt="Up trending!" />
-              <p className="text-green-400">12 Today</p>
-            </div>
-          </Card>
-
-          <Card className="dark:bg-[#252A41] hover:dark:bg-[#3a4164] cursor-pointer border-t-4 border-x-0 border-b-0 !border-blue-300">
-            <div className="flex justify-center items-center">
-              <img className="w-7" src="/images/icon-twitter.svg" alt="Twitter Logo!" />
-              <p className="ps-3 text-sm dark:text-gray-400">@dantheman</p>
-            </div>
-            <div className="flex flex-col justify-center items-center py-2">
-              <h1 className="text-6xl font-bold">1044</h1>
-              <p className="text-gray-400">F O L L O W E R S</p>
-            </div>
-            <div className="flex justify-center items-center gap-1">
-              <img className="w-3 h-fit" src="/images/icon-up.svg" alt="Up trending!" />
-              <p className="text-green-400">42 Today</p>
-            </div>
-          </Card>
-
-          <div className="pt-1 h-5 bg-gradient-to-r from-[#FCB045] via-[#FD1D1D] to-[#833AB4] rounded-lg">
-          <Card className="dark:bg-[#252A41] hover:dark:bg-[#3a4164]">
-            <div className="flex justify-center items-center">
-              <img className="w-7" src="/images/icon-instagram.svg" alt="Instagram Logo!" />
-              <p className="ps-3 text-sm dark:text-gray-400">@dantheman</p>
-            </div>
-            <div className="flex flex-col justify-center items-center py-2">
-              <h1 className="text-6xl font-bold">11k</h1>
-              <p className="text-gray-400">F O L L O W E R S</p>
-            </div>
-            <div className="flex justify-center items-center gap-1">
-              <img className="w-3 h-fit" src="/images/icon-up.svg" alt="Up trending!" />
-              <p className="text-green-400">1205 Today</p>
-            </div>
-          </Card>
-          </div>
-
-          <Card className="dark:bg-[#252A41] hover:dark:bg-[#3a4164] cursor-pointer border-t-4 border-x-0 border-b-0 !border-red-600">
-            <div className="flex justify-center items-center">
-              <img className="w-7" src="/images/icon-youtube.svg" alt="Youtube Logo!" />
-              <p className="ps-3 text-sm dark:text-gray-400">DanTheMan</p>
-            </div>
-            <div className="flex flex-col justify-center items-center py-2">
-              <h1 className="text-6xl font-bold">9587</h1>
-              <p className="text-gray-400">S U B S C R I B E R S</p>
-            </div>
-            <div className="flex justify-center items-center gap-1">
-              <img className="w-3 h-fit" src="/images/icon-down.svg" alt="Up trending!" />
-              <p className="text-red-400">124 Today</p>
-            </div>
-          </Card> */}
 
     </div>
   )
--- components/ToggleDarkMode.tsx
+++ components/ToggleDarkMode.tsx
@@ -13,6 +13,17 @@ const ToggleDarkMode = () => {
         (theme == "dark") ? setIsDarkOn(true) : setIsDarkOn(false)
     }
 
+    const customTheme = {
+        toggle: {
+            base: "relative flex items-center rounded-full bg-gray-200", // Off state background
+            checked: {
+                on: "bg-linear-to-r from-blue-400  to-green-400 ", // Background color when ON
+                off: "bg-linear-to-r from-blue-400  to-green-400", // Background color when OFF
+            },
+        },
+        handle: "bg-white peer-checked:bg-gray-500",
+    };
+
     useEffect(() => {
         setIsDarkOn(theme == "light");
     })
@@ -22,7 +33,7 @@ const ToggleDarkMode = () => {
             <div className='flex flex-row'>
 
                 <label className='pe-3 text-black dark:text-gray-400' htmlFor="ToggleSwitch">Dark Mode</label>
-                <ToggleSwitch className='' color='lime' checked={isDarkOn} onChange={handleTheme} />
+                <ToggleSwitch className='' color='lime' theme={customTheme} checked={isDarkOn} onChange={handleTheme} />
                 {/* <Button onClick={() => setTheme("light")}>
                     Light
                 </Button>
